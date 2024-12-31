99e68675bdfeff928559fcdd5d205ade671c310c
Louis Ruble <louis.ruble@outlook.fr>
2024-12-31 12:35:24 +0100

    image 3

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -6,6 +6,7 @@ from configs import files_path as fp
 # Load the old image of the townhall and the last one I took
 img_1 = cv2.imread(fp.IMG_HOUSE_1)
 img_2= cv2.imread(fp.IMG_HOUSE_2)
+img_3 = cv2.imread(fp.IMG_HOUSE_3)
 
 #Load the calibration parameters
 intrinsics = np.load('configs/calibration_params.npz')
@@ -18,6 +19,9 @@ dist = intrinsics['dist']
 # Resize the images
 img_1 = cv2.resize(img_1, (752, 480))
 img_2 = cv2.resize(img_2, (752, 480))
+img_3 = cv2.resize(img_3, (752, 480))
+
+
 
 # Convert the images to grayscale           
 # img_old_gray = cv2.cvtColor(img_old, cv2.COLOR_BGR2GRAY)      
@@ -26,3 +30,4 @@ img_2 = cv2.resize(img_2, (752, 480))
 # Save the images   
 cv2.imwrite(fp.P_IMG_HOUSE_1, img_1)
 cv2.imwrite(fp.P_IMG_HOUSE_2, img_2)
+cv2.imwrite(fp.P_IMG_HOUSE_3, img_3)
