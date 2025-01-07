f2bbb66b3a8d976742df730a66b59b564bb86bcf
Louis Ruble <louis.ruble@outlook.fr>
2025-01-07 21:29:23 +0100

    old image resize

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,33 +1,13 @@
-import torch
 import cv2
 import numpy as np
 from configs import files_path as fp
 
 # Load the old image of the townhall and the last one I took
-img_1 = cv2.imread(fp.IMG_HOUSE_1)
-img_2= cv2.imread(fp.IMG_HOUSE_2)
-img_3 = cv2.imread(fp.IMG_HOUSE_3)
+old_house = cv2.imread(fp.OLD_IMG_PATH)
+new_house = cv2.imread(fp.P_IMG_HOUSE_1)
 
-#Load the calibration parameters
-intrinsics = np.load('configs/calibration_params.npz')
-F = intrinsics['mtx']
-dist = intrinsics['dist']
-
-# Undistort the images
-# img_1 = cv2.undistort(img_1, F, dist)
-# img_2 = cv2.undistort(img_2, F, dist)
-# Resize the images
-img_1 = cv2.resize(img_1, (752, 480))
-img_2 = cv2.resize(img_2, (752, 480))
-img_3 = cv2.resize(img_3, (752, 480))
+old_house = cv2.resize(old_house, (new_house.shape[1], new_house.shape[0]))
+cv2.imwrite(fp.P_OLD_IMG_PATH , old_house)
 
 
 
-# Convert the images to grayscale           
-# img_old_gray = cv2.cvtColor(img_old, cv2.COLOR_BGR2GRAY)      
-# img_new_gray = cv2.cvtColor(img_new, cv2.COLOR_BGR2GRAY)
-
-# Save the images   
-cv2.imwrite(fp.P_IMG_HOUSE_1, img_1)
-cv2.imwrite(fp.P_IMG_HOUSE_2, img_2)
-cv2.imwrite(fp.P_IMG_HOUSE_3, img_3)
